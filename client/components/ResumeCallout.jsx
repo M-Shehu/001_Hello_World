e11698fe86d23e04fb0435a5a20d5ee05a8a9512
Muhammad Shehu <muhammad.shehu@ramp.com>
2025-08-12 19:18:59 -0600

    Update ResumeCallout component to correct resume download link

--- client/components/ResumeCallout.jsx
+++ client/components/ResumeCallout.jsx
@@ -4,7 +4,12 @@ const ResumeCallout = () => (
   <section className="callout content-section">
     <div className="container text-center">
       <h2 className="mx-auto mb-5">Download My Resume!</h2>
-      <a className="btn btn-primary btn-xl" href="https://s3-us-west-1.amazonaws.com/shehu.dev/Shehu+CV_Graphical_General.pdf">Download Here</a>
+      <a
+        className="btn btn-primary btn-xl"
+        href="https://s3.us-west-1.amazonaws.com/shehu.dev/Shehu+CV.pdf"
+      >
+        Download Here
+      </a>
     </div>
   </section>
 );
